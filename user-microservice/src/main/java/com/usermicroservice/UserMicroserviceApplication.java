8df420efd161a25e2d09084eeea9fa64cce34704
David G <davidgar8879@gmail.com>
2023-08-28 18:34:53 -0600

    eureka

--- user-microservice/src/main/java/com/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/usermicroservice/UserMicroserviceApplication.java
@@ -1,21 +1,17 @@
 package com.usermicroservice;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 //indicamos con anotacion que este proyecto va a llamar a otros microservicios
 @EnableFeignClients
 @SpringBootApplication
-public class UserMicroserviceApplication implements CommandLineRunner {
+@EnableEurekaClient
+public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserMicroserviceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("UserMicroserviceApplication.run");
 	}
 }
